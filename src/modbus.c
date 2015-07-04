38d63e4130eeab9824c41b4a6a39997069372e96
grey <11829413@qq.com>
2015-07-04 10:59:39 +0800

    modbus写单个保持寄存器功能ok
    Signed-off-by: grey <11829413@qq.com>

--- src/modbus.c
+++ src/modbus.c
@@ -1,5 +1,6 @@
 #include "modbus.h"
-
+#include "variable.h"
+#include "Function.h"
 
 extern void Uart2TxSend(unsigned char c);
 extern void Uart2TxEn(void);
@@ -199,7 +200,7 @@ void modbus_receive(unsigned char c)
 }
 
 
-void modbus_read_registers(void)
+void modbus_read_holding_registers(void)
 {
 	unsigned short crc;
 	unsigned char cnt = (mb.request.data.read_single.count << 1) & 0x00FF;
@@ -211,24 +212,59 @@ void modbus_read_registers(void)
 	modbus_rd_regs(&mb.response.data[3], 
 					mb.request.data.read_single.address, 
 					mb.request.data.read_single.count);	
-	
+
 	crc = CRC16(&mb.response.data[0], cnt + 3);
-	mb.response.data[cnt + 3] = (crc & 0xFF00) >> 8;
-	mb.response.data[cnt + 4] = crc & 0x00FF;
+	mb.response.data[cnt + 3] = ((unsigned char *)&crc)[HIGH];
+	mb.response.data[cnt + 4] = ((unsigned char *)&crc)[LOW];
 
 	mb.response.length = cnt + 5;
 }
 
-void modbus_write_registers(void)
+void modbus_write_single_register(void)
 {
-	// write KeyParm
-	//Uart2Parm.KeyParamAddr = Uart2Parm.RxCurData;
-	//KeyParm.SaveParms[Uart2Parm.KeyParamAddr] = Uart2Parm.KeyParamDataL;
+	unsigned short crc;
+	unsigned short value;
 
-	// write EEPROM
-	//EEPROMADDR = 0xF000 + 2 * Uart2Parm.KeyParamAddr;
-    //EraseEE(__builtin_tblpage(&EPConfigS[0]),EEPROMADDR, WORD);
-    //WriteEE(&KeyParm.SaveParms[Uart2Parm.KeyParamAddr],__builtin_tblpage(&EPConfigS[0]),EEPROMADDR, WORD);
+	if (mb.request.data.write_single.address < 128) {
+		// write KeyParm
+		Uart2Parm.KeyParamAddr = mb.request.data.write_single.address;
+		KeyParm.SaveParms[Uart2Parm.KeyParamAddr] = mb.request.data.write_single.value;
+
+		// write EEPROM
+		EEPROMADDR = 0xF000 + 2 * Uart2Parm.KeyParamAddr;
+	    EraseEE(__builtin_tblpage(&EPConfigS[0]),EEPROMADDR, 1);
+	    WriteEE(&KeyParm.SaveParms[Uart2Parm.KeyParamAddr],__builtin_tblpage(&EPConfigS[0]),EEPROMADDR, 1);
+		ReadEE(__builtin_tblpage(&EPConfigS[0]), EEPROMADDR, (int *)&value, 1);
+		if (value == mb.request.data.write_single.value) {
+			// create response packet
+			mb.response.data[0] = mb.adr;
+			mb.response.data[1] = mb.request.function;
+			mb.response.data[2] = ((unsigned char *)&mb.request.data.write_single.address)[HIGH];
+			mb.response.data[3] = ((unsigned char *)&mb.request.data.write_single.address)[LOW];
+			mb.response.data[4] = ((unsigned char *)&value)[HIGH];
+			mb.response.data[5] = ((unsigned char *)&value)[LOW];
+			crc = CRC16(&mb.response.data[0], 6);
+			mb.response.data[6] = ((unsigned char *)&crc)[HIGH];
+			mb.response.data[7] = ((unsigned char *)&crc)[LOW];
+			mb.response.length = 8;
+		} else {
+			// write data error
+			mb.response.data[0] = mb.adr;
+			mb.response.data[1] = (MODBUS_EXCEPTION_SLAVE_OR_SERVER_FAILURE | 0x80); // data error
+			crc = CRC16(&mb.response.data[0], 2);
+			mb.response.data[2] = ((unsigned char *)&crc)[HIGH];
+			mb.response.data[3] = ((unsigned char *)&crc)[LOW];
+			mb.response.length = 4;
+		}
+	} else { //if (mb.request.data.write_single.address < 128)
+		// address invalid
+		mb.response.data[0] = mb.adr;
+		mb.response.data[1] = (MODBUS_EXCEPTION_ILLEGAL_DATA_ADDRESS | 0x80); // invalid address
+		crc = CRC16(&mb.response.data[0], 2);
+		mb.response.data[2] = ((unsigned char *)&crc)[HIGH];
+		mb.response.data[3] = ((unsigned char *)&crc)[LOW];
+		mb.response.length = 4;
+	}
 }
 
 
@@ -250,15 +286,16 @@ void modbus_slave(void)
 				// make response packet
 				switch (mb.request.function) {
 					case MODBUS_FC_READ_HOLDING_REGISTERS:
-					case MODBUS_FC_READ_INPUT_REGISTERS:
 						Uart2TxEn();
-						modbus_read_registers();
+						modbus_read_holding_registers();
 						break;
 
 					case MODBUS_FC_WRITE_SINGLE_REGISTER:
-					case MODBUS_FC_WRITE_MULTIPLE_REGISTERS:
 						Uart2TxEn();
-						modbus_write_registers();
+						modbus_write_single_register();
+						break;
+
+					case MODBUS_FC_WRITE_MULTIPLE_REGISTERS:
 						break;
 
 					default:
